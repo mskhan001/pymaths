a8f489c4036b6d43e0d1a975ae70739e4608ba0e
Prateek Yadav <prateekyadav@Prateeks-MacBook-Air.local>
2018-09-29 12:49:50 +0530

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,10 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-66-164-241.ap-south-1.compute.amazonaws.com',
+      host: 'ec2-18-191-25-224.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/pymaths.pem',
       ref: 'origin/master',
-      repo: 'git@bitbucket.org:shubhamgoyal001/pymaths.git',
+      repo: 'git@bitbucket.org:prateek964/pymaths.git',
       path: '/home/ubuntu/pymaths',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
