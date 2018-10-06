56c087681443ecd8ee1fac26d76c3d5f007a3303
Shubham Goyal <shubham.goyal@lowes.com>
2018-10-06 17:24:50 +0530

    https code

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express')
+var https = require('https');
+var fs = require('fs');
 const app = express()
+var options = {
+    key: fs.readFileSync('./privatekey.pem'),
+    cert: fs.readFileSync('./server.crt')
+};
 app.use(express.static('pymaths'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+https.createServer(options, app).listen(8443);
+// app.listen(3000, () => console.log('Server running on port 3000'))
