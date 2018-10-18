1bba1ccdd0857ff13f3d90cd64cdfb10f76bf125
Shubham Goyal <shubham.goyal@lowes.com>
2018-10-18 17:42:19 +0530

    Revert "https code"
    This reverts commit 56c087681443ecd8ee1fac26d76c3d5f007a3303.

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 const express = require('express')
-var https = require('https');
-var fs = require('fs');
 const app = express()
-var options = {
-    key: fs.readFileSync('./privatekey.pem'),
-    cert: fs.readFileSync('./server.crt')
-};
 app.use(express.static('pymaths'))
-
-https.createServer(options, app).listen(8443);
-// app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'))
