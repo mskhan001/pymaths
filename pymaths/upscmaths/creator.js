758e993579a6deb4309b38ec77a70a0d07ae8d96
Prateek Yadav <prateek96.4@gmail.com>
2018-11-08 23:07:22 +0530

    asset path changed

---
+++ pymaths/upscmaths/creator.js
@@ -0,0 +1,12 @@
+function loadFileAsText()
+{
+    var fileToLoad = document.getElementById("qwe").files[0];
+
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent)
+    {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        document.getElementById("inputTextToSave").value = textFromFileLoaded;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
