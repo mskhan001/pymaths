8e074e651943a8efa0277809e261f85967d08c3e
foxenn_pro <niky.uma@gmail.com>
2018-11-19 23:32:46 +0530

    fdasf

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-	module.exports = {mongoURI: ''}
+	module.exports = {mongoURI: 'mongodb://pymathsuser:pymaths123@ds119422.mlab.com:19422/pymaths'}
 } else {
-	module.exports = {mongoURI: 'mongodb://localhost/pymaths'}
+	module.exports = {mongoURI: 'mongodb://pymathsuser:pymaths123@ds119422.mlab.com:19422/pymaths'}
 }
